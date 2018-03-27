7aa1eb644a0973361da554449b41a242dc921b53
SaphiraR <saphirarozas@gmail.com>
2018-03-27 16:31:10 +0200

    Add files via upload

---
+++ SysExpTriangle/IFact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysExpTriangle
+{
+    public interface IFact
+    {
+        String Nom();
+        Object Valeur();
+        int Niveaux();
+        String Question();
+
+        void SetNiveaux(int p);
+    }
+}
